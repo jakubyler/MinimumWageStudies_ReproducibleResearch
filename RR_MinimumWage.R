7e2ef5c10756c24df2781bac21b42148859fef19
Jakub_Byler <j.byler@student.uw.edu.pl>
2021-05-06 19:47:34 +0200

    New links to papers.

--- RR_MinimumWage.R
+++ RR_MinimumWage.R
@@ -9,15 +9,15 @@ coefs = c(NULL,)
 # 4. Welch https://www.rand.org/pubs/papers/P5145.html
 # 5. Ragan 1977 http://links.jstor.org/sici?sici=0034-6535%28197705%2959%3A2%3C129%3AMWATYL%3E2.0.CO%3B2-X&origin=JSTOR-pdf
 # 6. Wachter&Kim https://papers.ssrn.com/sol3/papers.cfm?abstract_id=261237
-# 7. Iden
-# 8. Ragan 
-# 9. Abowd&Killingsworth
-# 10. Betsey&Dunson
-# 11. Brown
-# 12. Hammermesh
-# 13. Solon
-# 14. Wellington
-# 15. Klerman
+# 7. Iden NONE
+# 8. Ragan https://www.jstor.org/stable/1928808?seq=1
+# 9. Abowd&Killingsworth NONE
+# 10. Betsey&Dunson https://www.jstor.org/stable/1815749?seq=1
+# 11. Brown http://links.jstor.org/sici?sici=0022-166X%28198324%2918%3A1%3C3%3ATEOTEO%3E2.0.CO%3B2-Q&origin=JSTOR-pdf
+# 12. Hammermesh https://www.nber.org/system/files/working_papers/w0656/w0656.pdf
+# 13. Solon http://links.jstor.org/sici?sici=0022-166X%28198521%2920%3A2%3C292%3ATMWATE%3E2.0.CO%3B2-8&origin=JSTOR-pdf
+# 14. Wellington http://links.jstor.org/sici?sici=0022-166X%28199124%2926%3A1%3C27%3AEOTMWO%3E2.0.CO%3B2-5&origin=JSTOR-pdf
+# 15. Klerman NONE
 
 
 
