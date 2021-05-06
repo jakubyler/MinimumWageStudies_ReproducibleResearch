270e50541a4bb5dbee384c215bd99434b8f09f3a
Jakub_Byler <j.byler@student.uw.edu.pl>
2021-05-06 18:47:33 +0200

    First version of the file that creates data frame with dates and names of the authors form the original paper.

---
+++ RR_MinimumWage.R
@@ -0,0 +1,7 @@
+lp = c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+date = c(1970,1976,1976,1976,1977,1979,1980,1981,1981,1981,1983,1981,1985,1991,1992)
+author = c('Kaitz','Mincer','Gramlich','Welch','Ragan','Wachter&Kim','Iden','Ragan','Abowd&Killingsworth','Betsey&Dunson','Brown','Hammermesh','Solon','Wellington','Klerman')
+coefs = c()
+
+
+df = data.frame(lp,author,date)
